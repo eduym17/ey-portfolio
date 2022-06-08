399a6fc53088a43d58ca1ba391e84a1e9e1f2dcd
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-06-08 18:19:31 -0500

    chore: add icons to assets

---
+++ src/components/utils/icons.js
@@ -0,0 +1,9 @@
+import iconLogo from '../assets/icons/icon-logo.svg';
+import iconImago from '../assets/icons/icon-imago.svg';
+import iconLinkedIn from '../assets/icons/icon-linkedin.svg';
+import iconGitHub from '../assets/icons/icon-github.svg';
+import iconTwitter from '../assets/icons/icon-twitter.svg';
+
+export {
+  iconLogo, iconImago, iconLinkedIn, iconGitHub, iconTwitter,
+};
