b8e8304f511060c4132ff5e11769b2a3aae7ea98
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-06-09 18:20:28 -0500

    chore: create texts imports file

---
+++ src/components/utils/texts.js
@@ -0,0 +1,14 @@
+export const infoInSitu = {
+  title: 'In Situ Energía website',
+  description: 'For this project I was responsible for all the Front-End and Back-End, including UX/UI design, implementation and maintenance.',
+  stack: 'JavaScript • ReactJS • TailwindCSS • Redux • ViteJS • NodeJS • MongoDB',
+  website: 'https://www.in-situ.com.mx',
+  isWebsiteDisabled: true,
+  repo: '/',
+  isRepoDisabled: false,
+};
+
+export const infoDevStore = {
+  title: 'In Situ Energía website',
+  description: '•',
+};
