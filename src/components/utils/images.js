81152d6f006e57c75578bc86e67ae0a708c9ce2b
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-06-09 18:08:44 -0500

    chore: add and export images

---
+++ src/components/utils/images.js
@@ -0,0 +1,9 @@
+import asteroid from '../../assets/images/asteroid.svg';
+import newBody from '../../assets/images/newBody.svg';
+import inSitu from '../../assets/images/insitu.png';
+import devStore from '../../assets/images/devstore.png';
+import pokedex from '../../assets/images/pokedex.png';
+
+export {
+  asteroid, newBody, inSitu, devStore, pokedex,
+};
