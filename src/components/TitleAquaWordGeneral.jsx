89d94689c63f121289d2e3023d7ec6b20f48e45f
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-06-09 18:10:59 -0500

    feat: title aqua component

---
+++ src/components/TitleAquaWordGeneral.jsx
@@ -0,0 +1,24 @@
+import propTypes from 'prop-types';
+
+const TitleAquaWordGeneral = ({ title, magicWord, customClass }) => (
+  <p className={`w-full font-semibold text-3xl ${customClass}`}>
+    {title.split(' ').map((word) => (
+      word === magicWord
+        ? <span key={word} className="text-dev-aqua">{`${magicWord}`}</span>
+        : `${word} `))}
+  </p>
+);
+
+TitleAquaWordGeneral.propTypes = {
+  title: propTypes.string,
+  magicWord: propTypes.string,
+  customClass: propTypes.string,
+};
+
+TitleAquaWordGeneral.defaultProps = {
+  title: 'Section title here',
+  magicWord: 'here',
+  customClass: '',
+};
+
+export default TitleAquaWordGeneral;
