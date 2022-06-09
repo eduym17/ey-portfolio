6a69202831744d235c5e34cb59a9e9a944e253fa
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-06-09 18:12:07 -0500

    fix: icons imports routes

--- src/components/utils/icons.js
+++ src/components/utils/icons.js
@@ -1,8 +1,8 @@
-import iconLogo from '../assets/icons/icon-logo.svg';
-import iconImago from '../assets/icons/icon-imago.svg';
-import iconLinkedIn from '../assets/icons/icon-linkedin.svg';
-import iconGitHub from '../assets/icons/icon-github.svg';
-import iconTwitter from '../assets/icons/icon-twitter.svg';
+import iconLogo from '../../assets/icons/icon-logo.svg';
+import iconImago from '../../assets/icons/icon-imago.svg';
+import iconLinkedIn from '../../assets/icons/icon-linkedin.svg';
+import iconGitHub from '../../assets/icons/icon-github.svg';
+import iconTwitter from '../../assets/icons/icon-twitter.svg';
 
 export {
   iconLogo, iconImago, iconLinkedIn, iconGitHub, iconTwitter,
