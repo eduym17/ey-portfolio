6949d03937000b8c39ab6ec25c97461a6290d927
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-06-10 14:15:11 -0500

    chore: add new icons and export them

--- src/components/utils/icons.js
+++ src/components/utils/icons.js
@@ -3,7 +3,23 @@ import iconImago from '../../assets/icons/icon-imago.svg';
 import iconLinkedIn from '../../assets/icons/icon-linkedin.svg';
 import iconGitHub from '../../assets/icons/icon-github.svg';
 import iconTwitter from '../../assets/icons/icon-twitter.svg';
+import iconCSS3 from '../../assets/icons/icon-css3.svg';
+import iconExpress from '../../assets/icons/icon-express.svg';
+import iconFigma from '../../assets/icons/icon-figma.svg';
+import iconGit from '../../assets/icons/icon-git.svg';
+import iconHTML5 from '../../assets/icons/icon-html5.svg';
+import iconIllustrator from '../../assets/icons/icon-illustrator.svg';
+import iconJavaScript from '../../assets/icons/icon-javascript.svg';
+import iconJest from '../../assets/icons/icon-jest.svg';
+import iconMongoDB from '../../assets/icons/icon-mongodb.svg';
+import iconNodeJS from '../../assets/icons/icon-nodejs.svg';
+import iconPhotoshop from '../../assets/icons/icon-photoshop.svg';
+import iconPostgreSQL from '../../assets/icons/icon-postgresql.svg';
+import iconReact from '../../assets/icons/icon-react.svg';
+import iconVue from '../../assets/icons/icon-vuejs.svg';
 
 export {
-  iconLogo, iconImago, iconLinkedIn, iconGitHub, iconTwitter,
+  iconLogo, iconImago, iconLinkedIn, iconGitHub, iconTwitter, iconCSS3, iconExpress, iconFigma,
+  iconGit, iconHTML5, iconIllustrator, iconJavaScript, iconJest, iconMongoDB, iconNodeJS,
+  iconPhotoshop, iconPostgreSQL, iconReact, iconVue,
 };
