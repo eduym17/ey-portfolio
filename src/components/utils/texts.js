3cb750fe32359b88953fcac1420eb678dfe9bb68
Eduardo Yañez <eduardo_yanez@live.com.mx>
2022-06-10 10:38:09 -0500

    chore: create objects for projects info

--- src/components/utils/texts.js
+++ src/components/utils/texts.js
@@ -2,13 +2,28 @@ export const infoInSitu = {
   title: 'In Situ Energía website',
   description: 'For this project I was responsible for all the Front-End and Back-End, including UX/UI design, implementation and maintenance.',
   stack: 'JavaScript • ReactJS • TailwindCSS • Redux • ViteJS • NodeJS • MongoDB',
-  website: 'https://www.in-situ.com.mx',
-  isWebsiteDisabled: true,
-  repo: '/',
-  isRepoDisabled: false,
+  linkWebsite: 'https://www.in-situ.com.mx',
+  isWebDisabled: false,
+  linkRepo: '/',
+  isRepoDisabled: true,
 };
 
 export const infoDevStore = {
-  title: 'In Situ Energía website',
-  description: '•',
+  title: 'devStore website',
+  description: 'This website was created to simulate a real shopping cart where you can add or remove items from it.',
+  stack: 'JavaScript • ReactJS • CSS3',
+  linkWebsite: '/',
+  isWebDisabled: true,
+  linkRepo: 'https://github.com/eduym17/EY-shopping-cart',
+  isRepoDisabled: false,
+};
+
+export const infoPokedex = {
+  title: 'Pokedex API website',
+  description: 'I created this website to get data from PokeAPI service. This app is completely designed to mobile phone portviews.',
+  stack: 'HTML5 • CSS3 • JavaScript',
+  linkWebsite: 'https://eduym17.github.io/LX-FE-practice-pokedex/',
+  isWebDisabled: false,
+  linkRepo: 'https://github.com/eduym17/LX-FE-practice-pokedex',
+  isRepoDisabled: false,
 };
