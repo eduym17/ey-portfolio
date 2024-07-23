b4501266ac9f4973216771de111b23415d1c3b7f
Eduardo Yañez <eduardo_yanez@live.com.mx>
2024-07-22 21:37:51 -0600

    Update images.js

--- src/components/utils/images.js
+++ src/components/utils/images.js
@@ -1,5 +1,5 @@
 import asteroid from '../../assets/images/asteroid.svg';
-import newBody from '../../assets/images/newBody.svg';
+import newBody from '../../assets/images/newbody.svg';
 import inSitu from '../../assets/images/insitu.png';
 import devStore from '../../assets/images/devstore.png';
 import pokedex from '../../assets/images/pokedex.png';
