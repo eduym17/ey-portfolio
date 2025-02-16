84140361c54444b8b9207e57a782e8caa33cfbc7
Eduardo Yañez Mejía <eduym@192.168.0.225>
2025-02-16 16:22:55 -0600

    updates projects texts

--- src/components/utils/texts.js
+++ src/components/utils/texts.js
@@ -1,6 +1,6 @@
 export const infoInSitu = {
   title: 'In Situ Energía website',
-  description: 'For this project I was responsible for all the Front-End and Back-End, including UX/UI design, implementation and maintenance.',
+  description: 'For this project, I handled everything from Front-End to Back-End—including UX/UI design, development, and ongoing maintenance.',
   stack: 'JavaScript • ReactJS • TailwindCSS • Redux • ViteJS • NodeJS • MongoDB',
   linkWebsite: 'https://www.in-situ.com.mx',
   isWebDisabled: false,
@@ -10,7 +10,7 @@ export const infoInSitu = {
 
 export const infoDevStore = {
   title: 'devStore website',
-  description: 'This website was created to simulate a real shopping cart where you can add or remove items from it.',
+  description: 'This website lets you experience a real shopping cart in action! You can easily add or remove items, just like in an actual online store.',
   stack: 'JavaScript • ReactJS • CSS3',
   linkWebsite: 'https://eduym17.github.io/eyShoppingCart/build/',
   isWebDisabled: false,
@@ -20,7 +20,7 @@ export const infoDevStore = {
 
 export const infoPokedex = {
   title: 'Pokedex API website',
-  description: 'I created this website to get data from PokeAPI service. This app is completely designed to mobile phone portviews.',
+  description: "I built this website to fetch data from the PokeAPI service. It's fully optimized for mobile devices, ensuring a smooth and seamless experience.",
   stack: 'HTML5 • CSS3 • JavaScript',
   linkWebsite: 'https://eduym17.github.io/lxFePracticePokedex/',
   isWebDisabled: false,
